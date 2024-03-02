4e5465c4e4ed596f21b26b133a1808bb50739872
Guilherme <guilhermersouza34@gmail.com>
2024-03-02 12:22:10 -0300

    first commit

---
+++ ExemplosMod32/src/main/java/br/com/rpires/dao/ICursoDao.java
@@ -0,0 +1,14 @@
+package br.com.rpires.dao;
+
+import java.util.List;
+
+import br.com.rpires.domain.Curso;
+
+public interface ICursoDao {
+
+	public Curso cadastrar(Curso curso);
+
+	public void excluir(Curso cur);
+
+	public List<Curso> buscarTodos();
+}
